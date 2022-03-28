473df507deda48203ad7196373d5d39f4d756110
miguel plasencia <miguel_plasencia_18@hotmail.com>
2022-03-27 20:03:20 -0500

    arreglalo

--- main.js/main.js
+++ main.js/main.js
@@ -1,3 +1,4 @@
+/* script para detectar los frames del scroll */
 window.addEventListener('scroll', function(){
     let animation = this.document.getElementById('animation');
     let positionObj1 = animation.getBoundingClientRect().top;
