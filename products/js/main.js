27afd6869aa3b16b6598994a6707e283879164e0
miguel plasencia <miguel_plasencia_18@hotmail.com>
2022-03-30 13:18:41 -0500

    DOmainRegistration

---
+++ products/js/main.js
@@ -0,0 +1,6 @@
+/* Script de la animacion de carga indica cuando debe desaparecer */
+window.onload = function(){
+    var contenedor = document.getElementById('container-carga');
+    contenedor.style.visibility = 'hidden';
+    contenedor.style.opacity = '0';
+}
